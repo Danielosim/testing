f954f4dba668467165683eea55b93b67a4108320
Danielosim <86317406+Danielosim@users.noreply.github.com>
2021-06-23 01:15:58 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,32 @@
+OLED.init(64, 128)
+OLED.showNumberWithNewLine(0)
+OLED.showStringWithNewLine("Hello, OLED!")
+OLED.showNumberNoNewLine(0)
+OLED.showStringNoNewLine("Hello, OLED!")
+OLED.showProgress(0)
+OLED.drawLine(
+0,
+0,
+0,
+0
+)
+OLED.drawCircle(0, 0, 0)
+OLED.fillCircle(0, 0, 0)
+OLED.drawRectangle(
+0,
+0,
+0,
+0
+)
+OLED.fillRectangle(
+0,
+0,
+0,
+0
+)
+OLED.clear()
+OLED.loadingScreen()
+OLED.nextLine()
+basic.forever(function () {
+	
+})
